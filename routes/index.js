0cf2c39fc0bd7b7d2ff7565cd999121269cf4d01
Pranav Kural <pranavkural042@gmail.com>
2017-02-07 21:15:49 -0500

    Informing the visitors that site is in progress

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // sending the message to wait
+  res.render('index', { title: 'Pranav Kural', message: 'Hi, there! this is my portfolio website. It\'s still in construction but you can hope to see something cool soon :)' });
 });
 
 module.exports = router;
